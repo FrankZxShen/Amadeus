35a86e71a00a75f0d41eae78f00a3e76d2142bf9
ShaoWei Du <122151565+Congroooo@users.noreply.github.com>
2023-07-07 18:10:01 +0800

    Add files via upload

---
+++ ButtonClickController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ButtonClickController : MonoBehaviour
+{
+    public Animator modelAnimator; // 3D模型的动画控制器
+    public string specificActionStateName; // 特定动作的动画状态名称
+
+    private int specificActionStateHash; // 特定动作状态的哈希值
+
+    private void Start()
+    {
+        specificActionStateHash = Animator.StringToHash(specificActionStateName);
+    }
+
+    public void StartSpecificAction()
+    {
+        modelAnimator.CrossFade(specificActionStateHash, 0.1f);
+    }
+}
